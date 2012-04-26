ffda6bd8350dd47911ca0e6830b0e0740f9b980f
Gervel Giva <gervelgiva@gmail.com>
2012-04-26 01:45:42 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awesomeblog::Application.config.secret_token = '8821dda20e1ad88fa83a69b32c3bed7fcd373b77df880cea1ef14627cb6f8b63469532b19ac5592e5e82f28eaf6ea6a74da95b7c60d9a423f6e2175bda2b03fe'
