795a5bbe606a04bf0b3a21cab4b0938bb502bae9
Gervel Giva <gervelgiva@gmail.com>
2012-04-26 10:27:28 -0400

    Set up the validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,12 +24,18 @@ class PostsController < ApplicationController
   end
 
   def update
-
+    @post = Post.find(params[:id])
+    original_title = @post.title
+    if @post.update_attributes(params[:post])
+      redirect_to posts_path, :notice => "'#{original_title}' was updated!"
+    else
+      render "edit"
+    end
   end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path, :notice => "#{@post.title} was removed!"
+    redirect_to posts_path, :notice => "'#{@post.title}' was removed!"
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title
+
+  validates :title, :content, :presence => true;
+  validates :title, :uniqueness => true
+  validates :title, :length => {:minimum => 4}
 end
