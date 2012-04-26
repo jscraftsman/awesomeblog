35579c699d0f8e336fa33f83dc20a1f7c7755c95
Gervel Giva <gervelgiva@gmail.com>
2012-04-26 19:54:13 +0800

    Model is now added, you can view, edit and remove a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,35 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path, :notice => "Your post was added!"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, :notice => "#{@post.title} was removed!"
   end
 end
